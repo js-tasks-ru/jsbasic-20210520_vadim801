7db79e09aca634d7817ff18148207eb8a11fc95a
Vadim Lazarenko <vadim2886@gmail.com>
2021-06-02 09:56:06 +0300

    решение

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,17 @@
+
 function isEmpty(obj) {
   // ваш код...
+   
+  if ( JSON.stringify(obj) == "{}" ){
+    for (let key in obj) {
+      if(obj.prop === undefined) {
+     return false
+        consle.log("fds")
+     }  
+    }    
+    return true
+  }
+    
+  
+  return false
 }
