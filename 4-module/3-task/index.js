6901ccbbbb09b914820c1ce108f5f09a239e9969
Vadim Lazarenko <vadim2886@gmail.com>
2021-06-13 16:05:02 +0300

    решение

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,37 @@
 function highlight(table) {
-  // ваш код...
+
+  for (let i = 1; i < table.rows.length; i++){
+
+    let row = table.rows[i]
+
+      for (let a of row.cells){
+        
+        if(Number(a.textContent) < 18 ) {
+          console.log(a)
+          row.style.textDecoration = 'line-through';
+        }
+
+        if(a.textContent === 'm'){
+          row.classList.add('male');
+        } 
+        if (a.textContent === 'f') {
+          row.classList.add('female');
+        }
+
+        if(a.hasAttribute('data-available')){
+          
+          if (a.getAttribute('data-available') === 'true'){
+            row.classList.add('available');
+          } else {
+            row.classList.add('unavailable');
+          }
+
+        }else {
+          row.setAttribute('hidden', 'hidden')
+        }
+      }
+   
+
+  }
+
 }
